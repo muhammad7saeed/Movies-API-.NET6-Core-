02744abe86a4b8979f816f752a5b3bc8855a1cc5
muhammad7saeed <mido_bob200970@yahoo.com>
2022-08-24 03:42:56 +0200

    Add DB Context

---
+++ Models/ApplactionDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MoviesAPI.Models
+{
+    public class ApplactionDbContext : DbContext
+    {
+
+        public ApplactionDbContext(DbContextOptions<ApplactionDbContext> options) :base(options)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+
+
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnections");
+builder.Services.AddDbContext<ApplactionDbContext>(options =>
+options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
